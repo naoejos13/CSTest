c6b4dfd78525021abf0f99f9453eda66ea4af728
naoejos13 <83151402+naoejos13@users.noreply.github.com>
2023-02-27 10:16:06 +0800

    Add files via upload

---
+++ Class/newHashMap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSignalTest.Class
+{
+	public class NewHashMap
+	{
+		private Dictionary<int, int> newHM;
+
+		public void insert(int num1, int num2) {
+			newHM.Add(num1, num2);
+		}
+
+		public int get(int key) {
+			//search for hashmap using key
+			return newHM[key];
+		}
+
+		public void addToKey(int value) {
+			addValue(false,value);
+		}
+
+		public void addToValue(int value) {
+			addValue(true, value);
+		}
+
+		public void addValue(bool whereToAdd, int value) {
+			Dictionary<int, int> tmp = new Dictionary<int, int>();
+			foreach (KeyValuePair<int, int> pair in newHM)
+			{
+				int tmpKey=0;
+				if (whereToAdd) {
+					tmpKey = pair.Value + value;
+					tmp.Add(pair.Key, tmpKey);
+				}else{
+					tmpKey = pair.Key + value;
+					tmp.Add(tmpKey, pair.Value);
+				}			
+			}
+			newHM.Clear();
+			newHM = tmp;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeSignalTest.Class;
+
+namespace CodeSignalTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			NewHashMap HM = new NewHashMap();
+			HM.insert(1, 2);
+			HM.get(1);
+			HM.addToKey(3);
+			HM.addToValue(4);
+		}
+	}
+}
